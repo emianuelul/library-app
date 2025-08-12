f7f47deea7919af9bee22ca7cec50a4e93f282ca
emianuelul <emanuel.marin0000@gmail.com>
2025-08-12 15:34:57 +0300

    style: main style for page, books not included

---
+++ code.js
@@ -0,0 +1,11 @@
+const addBtn = document.getElementsByClassName(".addBtn");
+
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
