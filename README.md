66131457ed0f97173f8811d25fcd14bf6531a1bf
emianuelul <emanuel.marin0000@gmail.com>
2025-08-14 15:24:45 +0300

    feat: click on book to open modal showing details and delete option + readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # library-app
+
+Made this project to practice basic OOP in JavaScript. Ended up practicing both CSS layout design AND JavaScript.
+
+---
+
+### Guide
+
+- Click on `Add Book` to add a book to the bookshelf;
+- Click on book to view details you inputted and modify read status;
+- In the details modal, you can also click the trash can icon to delete the book you are currently viewing.
+
+---
+
+Making the page save content between refreshes is something I might do in the future.
--- code.js
+++ code.js
@@ -1,9 +1,27 @@
 const addBtn = document.querySelector('.addBtn');
+const bookcase = document.querySelector('.bookcase');
+
 // ====== FORM ITEMS ======
 const form = document.querySelector('form');
 const modal = document.querySelector('.modal');
 const closeBtn = document.querySelector('.closeBtn');
 const submitBtn = document.querySelector('.submitBtn');
+
+const bookDetailsModal = document.querySelector('.book-details-modal');
+const okBtn = document.querySelector('.okBtn');
+const delBtn = document.querySelector('.deleteBtn');
+const readCheck = document.querySelector('#update-read');
+
+const bookDetails = document.querySelectorAll('.detail-value');
+
+readCheck.addEventListener('mouseup', (event) => updateBooks(currentIndex));
+
+okBtn.addEventListener('mouseup', (event) => {
+  bookDetailsModal.classList.remove('open');
+});
+
+let currentIndex = 0;
+delBtn.addEventListener('mouseup', (event) => deleteBook(currentIndex));
 
 form.addEventListener('submit', (event) => addBookToLibrary(event));
 
@@ -43,9 +61,6 @@ function addBookToLibrary(event) {
   let book = new Book(dummyBook);
   myLibrary.push(book);
 
-  console.log(dummyBook);
-  console.log(book);
-
   updateRows(myLibrary.length);
 
   modal.classList.remove('open');
@@ -56,8 +71,6 @@ function addBookToLibrary(event) {
 function updateRows(size) {
   let index = size - 1;
   let rowindex = Math.floor(index / 10);
-
-  const bookcase = document.querySelector('.bookcase');
 
   let book = document.createElement('div');
   let title = document.createElement('div');
@@ -88,18 +101,46 @@ function updateRows(size) {
   bookcase.appendChild(book);
 
   book.addEventListener('mouseup', () => {
-    // remove element
-    const x = myLibrary.splice(index, 1);
-    bookcase.removeChild(book);
+    bookDetailsModal.classList.add('open');
+    currentIndex = index;
 
-    const books = document.querySelectorAll('.book');
-    let i = 0;
-    for (item of books) {
-      let rowindex = Math.floor(i / 10);
-      item.setAttribute('data-index', `${i}`);
-      item.setAttribute('data-row', `${rowindex}`);
-
-      i++;
+    if (myLibrary[currentIndex].read) {
+      readCheck.checked = true;
+    } else {
+      readCheck.checked = false;
     }
+
+    bookDetails[0].textContent = myLibrary[currentIndex].name;
+    bookDetails[1].textContent = myLibrary[currentIndex].author;
+    bookDetails[2].textContent = myLibrary[currentIndex].pages;
   });
+}
+
+function deleteBook(index) {
+  // remove element
+  let books = document.querySelectorAll('.book');
+  const x = myLibrary.splice(index, 1);
+  bookcase.removeChild(books[index]);
+
+  books = document.querySelectorAll('.book');
+  let i = 0;
+  for (item of books) {
+    let rowindex = Math.floor(i / 10);
+    item.setAttribute('data-index', `${i}`);
+    item.setAttribute('data-row', `${rowindex}`);
+
+    i++;
+  }
+}
+
+function updateBooks(index) {
+  const books = document.querySelectorAll('.book');
+
+  if (readCheck.checked) {
+    books[index].classList.remove('read');
+    myLibrary[index].read = false;
+  } else {
+    books[index].classList.add('read');
+    myLibrary[index].read = true;
+  }
 }
